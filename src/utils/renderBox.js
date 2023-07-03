4db6eb537764cb1292ef11a04964b3a7f6a8112b
FatemeZamanian <FatemehZamanian.1999@gmail.com>
2023-07-03 13:35:12 +0330

    draw lines

--- src/utils/renderBox.js
+++ src/utils/renderBox.js
@@ -1,4 +1,4 @@
-import labels from "./labels.json";
+
 
 /**
  * Render prediction boxes
@@ -11,105 +11,67 @@ import labels from "./labels.json";
 export const renderBoxes = (canvasRef, keypointData,xi,yi) => {
   const ctx = canvasRef.getContext("2d");
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
-  for (let i = 0; i < keypointData.length; i += 3) {
-    const x = keypointData[i] * xi;
-    const y = keypointData[i + 1] * yi;
+
+  const colors = {
+    nose: 'red',
+    leftEye: 'blue',
+    rightEye: 'green',
+    leftEar: 'orange',
+    rightEar: 'purple',
+    leftShoulder: 'yellow',
+    rightShoulder: 'pink',
+    leftElbow: 'cyan',
+    rightElbow: 'magenta',
+    leftWrist: 'lime',
+    rightWrist: 'indigo',
+    leftHip: 'teal',
+    rightHip: 'violet',
+    leftKnee: 'gold',
+    rightKnee: 'silver',
+    leftAnkle: 'brown',
+    rightAnkle: 'black'
+  };
+    for (let i = 0; i < keypointData.length; i += 3) {
+      const x = keypointData[i] * xi;
+      const y = keypointData[i + 1] * yi;
+      const bodyPart = Object.keys(colors)[i / 3];
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = colors[bodyPart];
+      ctx.fill();
+      ctx.closePath();
+    }
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = 'white';
+  const connections = [
+    ['nose', 'leftEye'],
+    ['nose', 'rightEye'],
+    ['leftEye', 'leftEar'],
+    ['rightEye', 'rightEar'],
+    ['leftShoulder', 'rightShoulder'],
+    ['leftShoulder', 'leftElbow'],
+    ['rightShoulder', 'rightElbow'],
+    ['leftElbow', 'leftWrist'],
+    ['rightElbow', 'rightWrist'],
+    ['leftShoulder', 'leftHip'],
+    ['rightShoulder', 'rightHip'],
+    ['leftHip', 'rightHip'],
+    ['leftHip', 'leftKnee'],
+    ['rightHip', 'rightKnee'],
+    ['leftKnee', 'leftAnkle'],
+    ['rightKnee', 'rightAnkle']
+  ];
+  for (const [partA, partB] of connections) {
+    const x1 = keypointData[Object.keys(colors).indexOf(partA) * 3] * xi;
+    const y1 = keypointData[Object.keys(colors).indexOf(partA) * 3 + 1] * yi;
+    const x2 = keypointData[Object.keys(colors).indexOf(partB) * 3] * xi;
+    const y2 = keypointData[Object.keys(colors).indexOf(partB) * 3 + 1] * yi;
+
     ctx.beginPath();
-    ctx.arc(x, y, 3, 0, 2 * Math.PI);
-    ctx.fillStyle = 'red';
-    ctx.fill();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
     ctx.closePath();
   }
-  
-
-//   const colors = new Colors();
-
-//   // font configs
-//   const font = `${Math.max(
-//     Math.round(Math.max(ctx.canvas.width, ctx.canvas.height) / 40),
-//     14
-//   )}px Arial`;
-//   ctx.font = font;
-//   ctx.textBaseline = "top";
-
-//   for (let i = 0; i < scores_data.length; ++i) {
-//     // filter based on class threshold
-//     const klass = labels[classes_data[i]];
-//     const color = colors.get(classes_data[i]);
-//     const score = (scores_data[i] * 100).toFixed(1);
-
-//     let [y1, x1, y2, x2] = boxes_data.slice(i * 4, (i + 1) * 4);
-//     x1 *= ratios[0];
-//     x2 *= ratios[0];
-//     y1 *= ratios[1];
-//     y2 *= ratios[1];
-//     const width = x2 - x1;
-//     const height = y2 - y1;
-
-//     // draw box.
-//     ctx.fillStyle = Colors.hexToRgba(color, 0.2);
-//     ctx.fillRect(x1, y1, width, height);
-
-//     // draw border box.
-//     ctx.strokeStyle = color;
-//     ctx.lineWidth = Math.max(Math.min(ctx.canvas.width, ctx.canvas.height) / 200, 2.5);
-//     ctx.strokeRect(x1, y1, width, height);
-
-//     // Draw the label background.
-//     ctx.fillStyle = color;
-//     const textWidth = ctx.measureText(klass + " - " + score + "%").width;
-//     const textHeight = parseInt(font, 10); // base 10
-//     const yText = y1 - (textHeight + ctx.lineWidth);
-//     ctx.fillRect(
-//       x1 - 1,
-//       yText < 0 ? 0 : yText, // handle overflow label box
-//       textWidth + ctx.lineWidth,
-//       textHeight + ctx.lineWidth
-//     );
-
-//     // Draw labels
-//     ctx.fillStyle = "#ffffff";
-//     ctx.fillText(klass + " - " + score + "%", x1 - 1, yText < 0 ? 0 : yText);
-//   }
-// };
-
-// class Colors {
-//   // ultralytics color palette https://ultralytics.com/
-//   constructor() {
-//     this.palette = [
-//       "#FF3838",
-//       "#FF9D97",
-//       "#FF701F",
-//       "#FFB21D",
-//       "#CFD231",
-//       "#48F90A",
-//       "#92CC17",
-//       "#3DDB86",
-//       "#1A9334",
-//       "#00D4BB",
-//       "#2C99A8",
-//       "#00C2FF",
-//       "#344593",
-//       "#6473FF",
-//       "#0018EC",
-//       "#8438FF",
-//       "#520085",
-//       "#CB38FF",
-//       "#FF95C8",
-//       "#FF37C7",
-//     ];
-//     this.n = this.palette.length;
-//   }
-
-//   get = (i) => this.palette[Math.floor(i) % this.n];
-
-//   static hexToRgba = (hex, alpha) => {
-//     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-//     return result
-//       ? `rgba(${[parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)].join(
-//           ", "
-//         )}, ${alpha})`
-//       : null;
-//   };
 }
 
